f8b754e00037de02cea96b7620a1cb421b5e5581
Sanjay <58023898+Sanjay-RD@users.noreply.github.com>
2020-04-23 19:39:56 +0545

    Add files via upload

---
+++ one/java.js
@@ -0,0 +1,14 @@
+console.log('rd');
+// alert('danger');
+	
+// var name =prompt('Enter your name');
+// console.log(name);
+
+document.getElementById('changeText').innerHTML = 'sanjay Rd';
+
+function text(){
+	console.log('sanjay');
+}
+
+text();
+
